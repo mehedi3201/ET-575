0f86fcf351625546920b637d077f5e89fa5169e1
Mehedi Mithu <mehedihassanmi6@gmail.com>
2025-09-03 20:06:59 +0000

    lab 1 complete

--- Lab1_variables/LAB_1mithu.cpp
+++ Lab1_variables/LAB_1mithu.cpp
@@ -31,6 +31,31 @@ int main()
     cout<<"Given distance = "<<distance<<endl;
     cout<<"Given number ="<<number<<endl;
 
+ // Declare variables
+    string country;
+    char gender;
+    int zipcode;
+    double rate;
+
+    cout<<"\n ------------ EXERCISE ------------ "<<endl;
+    // Ask for user input
+    cout << "Enter your country: ";
+    cin >> country;
+
+    cout << "Enter your gender (f for female, m for male, o for others): ";
+    cin >> gender;
+
+    cout << "Enter your zipcode: ";
+    cin >> zipcode;
+
+    cout << "Enter your tax rate (with decimal): ";
+    cin >> rate;
+
+    // Display the results
+    cout << "\nEntered country:\t" << country << endl;
+    cout << "Selected gender:\t" << gender << endl;
+    cout << "Zip code:\t\t" << zipcode << endl;
+    cout << "Tax rate:\t\t" << rate << endl;
 
     
     
