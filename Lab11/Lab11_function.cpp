ca683caf444a23e2310fdf7bd5c687dca18b2171
Mehedi Mithu <mehedihassanmi6@gmail.com>
2025-10-22 19:39:52 +0000

    done 11

---
+++ Lab11/Lab11_function.cpp
@@ -0,0 +1,14 @@
+/*
+Mehedi mithu
+Oct22, 2025
+Lab 11, introduction to function*/
+
+
+
+#include<iostream>
+
+// Void function does not return any value
+
+void printhello(){
+    cout<<"Hello function!"<<endl;
+}
---
+++ Lab11/lab11_main.cpp
@@ -0,0 +1,7 @@
+/*
+Mehedi mithu
+Oct22, 2025
+Lab 11, introduction to function*/
+
+#include<iostrem>
+#inculde"Lab11_function.cpp"
