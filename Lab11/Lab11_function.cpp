30dae4d5f5ff5d15d0adc7721e2bc24cb6785041
Mehedi Mithu <mehedihassanmi6@gmail.com>
2025-10-22 22:45:19 +0000

    11 done

--- Lab11/Lab11_function.cpp
+++ Lab11/Lab11_function.cpp
@@ -1,14 +1,93 @@
 /*
-Mehedi mithu
-Oct22, 2025
-Lab 11, introduction to function*/
+Mehedi Mithu
+Oct 22, 2025
+Lab 11, Introduction to functions
+*/
 
+#include <iostream>
 
+using namespace std;
 
-#include<iostream>
-
-// Void function does not return any value
-
+// void function does not return any value
 void printhello(){
-    cout<<"Hello function!"<<endl;
+    cout << "Hello functions!"<<endl;
+    return;
+}
+
+// void function with parameters
+void greeting(string name){
+    cout << "Good afternoon " << name << endl;
+    return;
+}
+
+// example 3, function that returns an interger(whole number)
+int returnfive(){
+    return 5;
+}
+
+// example 4, any code after the return will not be executed
+void printsome(){
+    cout<<"Something"<<endl;
+    return;
+    cout<<"After the return" << endl; // This line will not be executed
+}
+int somenumber(){
+    int num = 10;
+    return num; // code after this line will not be executed
+    num *= 2;
+    return num;
+}
+
+// example 5, function that calculates and returns the area of a square
+float areaSquare(float side){
+    float area = side * side;
+    return area;
+}
+
+// example 6, function that calculates and returns the product of two numbers
+int product(int n1, int n2){
+    return n1 * n2;
+}
+
+// example 7, function that calculates and returns the farenheit temperature
+double fah(double celsius){
+    return celsius * 1.8 + 32.0;
+}
+
+void printfah(double f){
+    cout<<"The farenheit temperature is: "<< f << endl;
+    return;
+}
+
+// example 8, check a number
+string checknumber(int number){
+    if(number == 0){
+        return "Zero";
+    }
+    else if(number > 0){
+        return "Positive";
+    }
+    else{
+        return "Negative";
+    }
+}
+
+void printnumber(int x, string n){
+    cout<<"Number "<< x << " Is "<< n << endl;
+    return;
+}
+
+// Exercise functions
+bool checkeven(int a){
+    if(a%2 == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void printeven(int number, bool result){
+    cout<<"Is "<< number << " Even? " << result << endl;
+    return;
 }
--- Lab11/lab11_main.cpp
+++ Lab11/lab11_main.cpp
@@ -1,7 +1,63 @@
 /*
-Mehedi mithu
-Oct22, 2025
-Lab 11, introduction to function*/
+Mehedi Mithu
+Oct 22, 2025
+Lab 11, Introduction to functions
+*/
 
-#include<iostrem>
-#inculde"Lab11_function.cpp"
+#include <iostream>
+#include "lab11_function.cpp"
+
+using namespace std;
+
+int main(){
+    cout<<"\n-----Example 1: void function -----"<<endl;
+    // call the void function
+    printhello(); 
+    printhello(); 
+    printhello();
+    
+    cout<<"\n-----Example 2: void function with arguments -----"<<endl;
+    string username = "Anna";
+    greeting("Peter");
+    greeting(username);
+    greeting("Tom");
+
+    cout<<"\n-----Example 3: function with returning value " << endl;
+    int  savefive = returnfive();
+    cout<< "The returning value is             \t"<< savefive << endl;
+    savefive += 2;
+    cout << "The returning value after += 2 is \t"<< savefive << endl;
+
+
+    cout << "\n-----Example 4: code after the return" << endl;
+    printsome();
+    int n = somenumber();
+    cout<< "Some number = "<<n<<endl;
+
+
+    cout << "\n-----Example 5: calculates are of a square" << endl;
+    float area_square = areaSquare(2.5);
+    cout<<"The area of a square is " << area_square << endl;
+
+
+    cout << "\n-----Example 6: product of two numbers" << endl;
+    int p = product(2,3);
+    cout<<"The product is "<< p << endl;
+
+    cout << "\n-----Example 7: calculates the farenheit temperature" << endl;
+    double f = fah(12.5);
+    printfah(f);
+    
+    cout<<"\n------Example 8: check a number -----"<<endl;
+    int x = 3;
+    string checknum = checknumber(x);
+    printnumber(x, checknum);
+
+
+    cout<<"\n-----Exercise -----"<<endl;
+    int a = 6;
+    bool check_e = checkeven(a);
+    printeven(a, check_e);
+
+    return 0;
+}
