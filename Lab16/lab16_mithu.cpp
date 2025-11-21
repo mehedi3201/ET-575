d306cc5bc2f5b0d0cc812d934689d9068843f429
Mehedi Mithu <mehedihassanmi6@gmail.com>
2025-11-21 01:15:11 +0000

    lab 16 done

--- Lab16/lab16_mithu.cpp
+++ Lab16/lab16_mithu.cpp
@@ -1,114 +1,38 @@
 /*
-Mehedi Mithu
+QiuLiangJun Li
 lab 16. array in a function
 Nov 17, 2025
 */
 
 #include <iostream>
+#include "lab16_function.cpp"
 
 using namespace std;
 
-// Example 1: function to populate an array with numbers.
-// The numbers are collected from the user.
-void fillup(int size_array , int array[])
-{
-    for (int i = 0; i < size_array; i++)
-    {
-        cout << "Enter a number: ";
-        cin >> array[i];
-    }
-    cout << endl;
-}
+int main(){
+    cout << "\n-----Example 1: Collect and populate data into an array -----" << endl;
+    // Initialize variables and array
+    int arraysize = 3;
+    int arrnum[arraysize];
+    // Calling functions
+    fillup(arraysize, arrnum);
+    printarray(arraysize, arrnum);
 
-// Function to print values in an array.
-void printarray(int size_array , int array[]){
-    for (int i = 0; i < size_array; i++)
-    {
-        cout << array[i] << "\t";
-    }
-    cout << endl;
-}
+    cout << "\n-----Example 2: Program to search a number in an array -----" << endl;
+    // Initialize variables and array
+    int arr_pos_numbers[ARRAY_SIZE];
+    int listsize = 0;
+    int targetnumber = 8;
+    // calling functions
+    fillarray(arr_pos_numbers, listsize);
+    printarray(listsize, arr_pos_numbers); // Testing if the
+    int resultsearch = search(arr_pos_numbers, listsize, targetnumber);
+    printresult(resultsearch, targetnumber);
 
-// Example 2: collect positive numbers (max 10 numbers), and search if a number exists amount the collected numbers.
-// function to collect non-negative number (max 10 numbers). If a negative number is entered, the function will stop
-const int ARRAY_SIZE = 10;
-
-void fillarray(int *array, int &number_user_index){
-    int number, index = 0;
-
-    do{
-        cout << "Enter a positive number: ";
-        cin >> number;
-        if(number > 0)
-        {
-            array[index] = number;
-            index++;
-        }
-
-    }while(number > 0 && index < ARRAY_SIZE);
-
-    // update the value of variable number_user_index, which is the last index of all positive numbers
-    number_user_index = index;
-
-}
-
-// Search function to search for a number in the array
-int search(const int *arr, int number_user_index, int target)
-{
-    int index = 0;
-    bool found = false;
-
-    while((!found) && (index < number_user_index))
-    {
-        if(target == arr[index])
-        {
-            found = true;
-        }
-        else
-        {
-            index++;
-        }
-    }
-
-    if(found)
-      return index;
-    else
-        return -1;
-}
-
-// Function to print result
-void printresult(int resultsearch, int target)
-{
-    cout << "What is the index of number " << target << " in the array? " << resultsearch << endl;
-}
-
-//EXERCISE
-// Function that returns the sum of all even numbers in an array
-int sum_even_numbers(int *arr, int size_array){
-    int sum = 0;
-    for (int i = 0; i < size_array; i++)
-    {
-        if(arr[i] % 2 == 0){
-            sum += arr[i];
-        }    
-    }
-    return sum;
-} 
-// function that returns the counts of all negative numbers in an array
-int count_negative(int *arr, int size_array){
-    int count = 0;
-    for (int i = 0; i < size_array; i++)
-    {
-        if(arr[i] < 0)
-        {
-            count++;
-        }
-    }
-    return count;
-}
-
-// Print result
-void print_result_exercise(int sum_even, int count_neg){
-    cout << "Sum of all even numbers in the array: " << sum_even << endl;
-    cout << "Count of negative numbers in the array: " << count_neg << endl;
+    cout << "\n-----EXERCISE -----" << endl;
+    int arr_numbers[]= {6,-10,9,12,-3,-8,5,7,4,0};
+    int arr_size = sizeof(arr_numbers) / sizeof(int);
+    
+    print_result_exercise(sum_even_numbers(arr_numbers,arr_size), count_negative(arr_numbers,arr_size));
+    return 0;
 }
